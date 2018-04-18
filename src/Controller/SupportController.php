4ecf1d6f6f6c17894cad59088786fa81b235d16f
DonCuicui <alexanrre.quilan@gmail.com>
2018-04-18 10:23:39 +0200

    Controller du support

---
+++ src/Controller/SupportController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SupportController extends Controller
+{
+    /**
+     * @Route("/support/submit", name="supportSubmit")
+     */
+    public function SubmitTicket()
+    {
+        return $this->render('support/index.html.twig', [
+            'controller_name' => 'Creer le Ticket',
+        ]);
+    }
+    /**
+     * @Route("/support", name="supportMessage")
+     */
+    public function MessageTicket()
+    {
+        return $this->render('support/index.html.twig', [
+            'controller_name' => 'Ouvre le ticket',
+        ]);
+    }
+    /**
+     * @Route("/support/close", name="supportSubmit")
+     */
+    public function CloseTicket()
+    {
+        return $this->render('support/index.html.twig', [
+            'controller_name' => 'ferme le Ticket',
+        ]);
+    }
+}

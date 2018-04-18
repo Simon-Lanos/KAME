b22bd8f59cf51c113b7957fa2254b37ea19959e6
Clement <clement.langendorf@gmail.com>
2018-04-18 17:28:37 +0200

    DefaultController + Mail forgottenPassword + Entity User avec Bruno et Kevin

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -21,14 +23,69 @@ class DefaultController extends Controller
         $form = $this->createFormBuilder()
             ->add("userMail", EmailType::class, array('label' => "Login : "))
             ->add("userPassword", PasswordType::class, array('label' => "Mot de Passe : "))
-            ->add("log", SubmitType::class, array('label' => "Se connecter : "))
+            ->add("log", SubmitType::class, array('label' => "Se connecter"))
             ->getForm();
 
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
 
-      return $this->render('default/index.html.twig', array(
-          'form' => $form->createView()
-      ));
+            $data = $form->getData();
+
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy(
+                    ['userMail' => $data["userMail"]]
+                );
+            if ($data['userPassword'] == $user->getUserPassword()) {
+                $session->set('user', $user);
+                return $this->redirectToRoute('default');
+            }
+        }
+
+
+        return $this->render('default/index.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+
+    /**
+     * @Route("/forget", name="forget")
+     */
+    public function forgottenPassword(Request $request, \Swift_Mailer $mailer)
+    {
+        $form = $this->createFormBuilder()
+            ->add("userMail", EmailType::class, array('label' => "Mail : "))
+            ->add("log", SubmitType::class, array('label' => "Envoyer"))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy(
+                    ['userMail' => $data["userMail"]]
+                );
+            if ($data['userMail'] == $user->getUserMail()){
+                $message = (new \Swift_Message('Hello Email'))
+                    ->setFrom('sendfrom@example.com')
+                    ->setTo($user->getUserMail())
+                    ->setBody('ceci est le body du mail');
+                $mailer->send($message);
+                return $this->redirectToRoute('default');
+            }
+           /* else
+                return $this->render('default/forget.html.twig', array(
+                    'form' => $form->createView()));*/
+
+        }
+
+        return $this->render('default/forget.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }

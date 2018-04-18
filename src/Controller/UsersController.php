22e697e358d5dfa4e47a5c12a38566245730d0c7
Alban Guerbette <aguerbette@gmail.com>
2018-04-18 17:04:16 +0200

    Création du formulaire pour la modification d'un utilisateur

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,8 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Users;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\HttpFoundation\Request;
 
 class UsersController extends Controller
 {
@@ -36,11 +45,29 @@ class UsersController extends Controller
             );
         }
 
+        $form = $this->createFormBuilder($user)
+            ->add('userMail', EmailType::class)
+            ->add('userPassword', PasswordType::class)
+            ->add('userFirstName', TextType::class)
+            ->add('userLastName', TextType::class)
+            ->add('userGender', ChoiceType::class)
+            ->add('userAdress', TextType::class)
+            ->add('userAvatar', TextType::class)
+            ->add('zipCode', IntegerType::class)
+            ->add('userCity', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Enregistrer mes changements'))
+            ->getForm();
+
+        return $this->render('users/profile_management.html.twig', array(
+            'form' => $form->createView(),
+            'user' => $user
+        ));
+
         //return new Response('Check out this great user: '.$user->getUserFirstName());
 
         // or render a template
         // in the template, print things with {{ user.name }}
-        return $this->render('users/profile_management.html.twig', ['user' => $user]);
+        //return $this->render('users/profile_management.html.twig', ['user' => $user]);
 
     }
 

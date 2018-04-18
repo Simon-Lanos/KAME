06a3401debc6d6a0a5b6280e9b5a67f03f712fc9
Thomas Robveille <thomas.ro@hotmail.fr>
2018-04-18 11:11:04 +0200

    Controller réalisé par Xavier - problème de connection avec GitHub

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thomas
+ * Date: 18/04/2018
+ * Time: 11:08
+ */
+
+namespace App\Controller;
+
+
+class ReservationController
+{
+    public function  RoomReservation(){
+        echo "RoomReservation est OK !";
+    }
+
+    public function EquipementReservation(){
+        echo "EquipementReservation est OK !";
+    }
+
+    public function  EquipementRestitution(){
+        echo "EquipementRestitution est OK !";
+    }
+
+}
